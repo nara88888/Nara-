5d0498d1aaeb7a021f9a5620fa54206dbb302e58
nara88888 <lilougoncalvesteixeira2@gmail.com>
2026-08-08 18:58:12 +0200

    Initialize Discord bot client with intents

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const { Client, GatewayIntentBits } = require('discord.js');
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`✅ ${client.user.tag} est en ligne !`);
+});
+
+client.login(process.env.TOKEN);
